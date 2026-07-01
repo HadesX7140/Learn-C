95445c371e49cd6dfdc1243588f2b3a71b719db0
Hades <piastaha02@gmail.com>
2026-07-01 19:10:59 +0600

    Refactor horseshoe.c for better readability

--- horseshoe.c
+++ horseshoe.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
-void main(void){
-    int a,b,c,d,count=0;
+void main(){
+    int a,b,c,d;
     printf("Enter the value of a,b,c,d:\n");
     scanf("%d %d %d %d",&a,&b,&c,&d);
     if(a==b && b==c && c==d)
@@ -10,7 +10,7 @@ void main(void){
     printf("count=2");
     else if(a==b && c==d || a==c && b==d || a==d && b==c)
     printf("count=2");
-    else if(a==b && a!=c && a!=d || b==c && b!=a && b!=d || c==d && c!=a && c!=b || a==d && a!=b && a!=c)
+    else if(a==b && a!=c && a!=d || a==c && a!=b && a!=d || a==d && a!=b && a!=c || b==c && b!=a && b!=d || b==d && b!=a && b!=c || c==d && c!=a && c!=b)
     printf("count=1");
     else printf("You r gud to go!");
 }
