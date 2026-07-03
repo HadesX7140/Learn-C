e8e86b4257e44f1dd6374de156fefea2c836b4f7
Hades <piastaha02@gmail.com>
2026-07-03 16:56:12 +0600

    Optimized bubble sorting

--- sorting.c
+++ sorting.c
@@ -8,8 +8,8 @@ printf("enter %d numbers:\n",n);
 for(i=0;i<n;i++){
     scanf("%d",&a[i]);
 }
-for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
+for(i=0;i<n-1;i++){
+    for(j=0;j<n-1-i;j++){
         if(a[j]>a[j+1]){
         temp=a[j];
         a[j]=a[j+1];
