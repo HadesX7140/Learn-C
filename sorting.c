3a2bd8ae7f79e4f6021eb573d27848e3a3a48cb3
Hades <piastaha02@gmail.com>
2026-07-03 13:16:04 +0600

    Add files via upload

---
+++ sorting.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+int a[50],n,i,j,temp;
+printf("enter total numbers:");
+scanf("%d",&n);
+printf("\n");
+printf("enter %d numbers:\n",n);
+for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+        if(a[j]>a[j+1]){
+        temp=a[j];
+        a[j]=a[j+1];
+        a[j+1]=temp;
+    }
+}
+}
+printf("ascending sorting: ");
+for(i=0;i<n;i++){
+    printf("%d\t",a[i]);
+}
+return 0;
+
+}
+
